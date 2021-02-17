612f6b4d5b23bd7516069a241174fcd7ac8fbef5
Peter Bui <pbui@nd.edu>
2021-02-17 09:47:42 -0500

    lecture07: add command line parsing to repeat

--- lecture07/repeat.sh
+++ lecture07/repeat.sh
@@ -1,15 +1,50 @@
 #!/bin/sh
 
-N=1
+# Gloabls
 
-if [ $# -ge 1 ]; then
-    N=$1
+N=1
+SHOUT=0
+
+# Functions
+
+usage() {				    # Review: I/O redirection, here doc
+    cat 1>&2 <<EOF
+Usage: repeat [-n N -s] MESSAGE
+    -n N    Repeat MESSAGE N times (default is 1)
+    -s      Shout MESSAGE (uppercase)
+EOF
+    exit $1
+}
+
+shout() {
+    if [ $SHOUT -eq 0 ]; then		    # Discuss: testing integers
+    	echo $@				    # Discuss: function arguments
+    else
+    	echo $@ | tr a-z A-Z		    # Discuss: translate
+    fi
+}
+
+# Parse Command Line Arguments
+
+while [ $# -gt 1 ]; do
+    case $1 in
+	-n) N=$2; shift ;;		    # Discuss: double shift
+	-s) SHOUT=1 ;;			    # Discuss: flags
+	-h) usage 0 ;;
+	-*) usage 1 ;;
+	 *) break ;;
+    esac
     shift
-else
-    echo "Usage: $(basename $0) N message..."
-    exit 1
+done
+
+MESSAGE=$@
+
+# Main Execution
+
+if [ -z "$MESSAGE" ]; then		    # Discuss: testing strings
+    usage 1
 fi
 
 for i in $(seq $N); do
-    echo "$@"
+    shout "$@"
 done
