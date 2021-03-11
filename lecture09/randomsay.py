30801de45d9ae6cd105326e86d978a1ec7f60fc8
Peter Bui <pbui@nd.edu>
2021-03-11 12:48:35 -0500

    lecture09: update randomsay

--- lecture09/randomsay.py
+++ lecture09/randomsay.py
@@ -6,18 +6,22 @@ import sys
 
 # Constants
                                                 # Discuss: set data structure
-BLACKLIST = {'bong', 'sodomized', 'kiss', 'head-in', 'telebears'}
+NSFW = {'bong', 'sodomized', 'kiss', 'head-in', 'telebears'}
 
 # Main Execution
 
-characters = []                                 # Discuss: os.popen
-for index, line in enumerate(os.popen('cowsay -l')):
-    if not index:                               # Discuss: enumerate
-        continue
+def main():
+    characters = []                             # Discuss: os.popen
+    for index, line in enumerate(os.popen('cowsay -l')):
+        if not index:                           # Discuss: enumerate
+            continue
 
-    for character in line.split():              # Review: str.split
-        if character not in BLACKLIST:          # Review: searching collection
-            characters.append(character)        # Review: list.append
+        for character in line.split():          # Review: str.split
+            if character not in NSFW:           # Review: searching collection
+                characters.append(character)    # Review: list.append
+    
+    selected = random.choice(characters)
+    os.system(f'cowsay -f {selected}')          # Variant: check exist status
 
-selected = random.choice(characters)
-os.system(f'cowsay -f {selected}')              # Variant: check exist status
+if __name__ == '__main__':
+    main()
