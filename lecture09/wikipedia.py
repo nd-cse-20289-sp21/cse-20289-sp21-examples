d7cfaa790da87d6ffc421f27a8a46ae2b003788e
Peter Bui <pbui@nd.edu>
2021-03-14 12:29:40 -0400

    Lecture 09: update wikipedia example

--- lecture09/wikipedia.py
+++ lecture09/wikipedia.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 
 import pprint
-import requests
+import re
 import sys
 
-url      = 'https://en.wikipedia.org/w/api.php' # Discuss: APIs
-                                                # Discuss: join strings
-params   = {'action': 'query', 'list': 'search', 'format': 'json', 'srsearch': sys.argv[1]}
-result   = requests.get(url, params=params)     # Review: keyword arguments
-data     = result.json()                        # Discuss: json
+import requests
 
-#pprint.pprint(data)                            # Discuss: pretty printing
+# Constants
 
-articles = data['query']['search']              # Discuss: sorting by key, lambda
-#articles = sorted(data['query']['search'], key=lambda o: o['wordcount'])
+URL      = 'https://en.wikipedia.org/w/api.php' # Discuss: APIs
+PARAMS   = {
+    'action'  : 'query', 
+    'list'    : 'search', 
+    'format'  : 'json', 
+    'srsearch': sys.argv[1]
+}
 
-for index, entry in enumerate(articles, 1):
-    print(f"{index:4}. {entry['title']}")
+# Main Execution
+
+def main():
+    result   = requests.get(URL, params=PARAMS) # Review: keyword arguments
+    data     = result.json()                    # Discuss: json
+
+    #pprint.pprint(data)                        # Discuss: pretty printing
+
+    articles = data['query']['search']          # Discuss: sorting by key, lambda
+    articles = sorted(data['query']['search'], key=lambda o: o['wordcount'])
+
+    for index, entry in enumerate(articles[:5], 1):
+        if index > 1:                           # Review: slicing, enumerate
+            print()                             # Discuss: fence pattern
+
+        title   = entry['title']                # Discuss: re.sub
+        snippet = re.sub(r'<[^>]+>', '', entry['snippet'])
+        print(f'{index:4}.\t{title}\n\t{snippet}')
+
+if __name__ == '__main__':
+    main()
